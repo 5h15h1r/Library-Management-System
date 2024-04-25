f1eafd73ae1e95ba02191a348a7d898bff86dcd4
5h15h1r <shishir.agrahari14@gmail.com>
2024-04-25 18:49:31 +0530

    cleaned code and made utils

--- app/server/middleware/rate_limiter.py
+++ app/server/middleware/rate_limiter.py
@@ -2,7 +2,7 @@ import redis
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
 from datetime import datetime
-import time
+from server.utils.getTime import timeInSeconds
 
 
 class RateLimiterMiddleware:
@@ -21,27 +21,19 @@ class RateLimiterMiddleware:
                 )
 
             current_date = datetime.utcnow()
-            # Calculate the end of the day
-            end_of_day = datetime(
-                current_date.year, current_date.month, current_date.day, 23, 59, 59
-            )
-            # Calculate local offset time
-            local_timezone_offset_seconds = time.timezone
-            remaining_time = end_of_day - current_date
-            expiration_time = remaining_time.seconds - +(-local_timezone_offset_seconds)
+            expiration = timeInSeconds(current_date)
 
             key = f"{user_id}"
             count = self.redis_client.get(key)
 
             # Check if the key exists
             if count is None:
-                # Key doesn't exist, set it with expiration
-                self.redis_client.set(key, 1, expiration_time)
-
                 # Assigning count = 1 for the edge case:  max_request = 1
                 count = 1
+                # If Key doesn't exist, set it with expiration time to the end of day
+                self.redis_client.set(key, count, expiration)
                 if count > self.max_requests:
-                    raise HTTPException(status_code=429, detail="Rate limit exceeded")
+                    raise HTTPException(status_code=429, detail="Rate uh limit exceeded")
                 response = await call_next(request)
                 return response
 
@@ -52,7 +44,6 @@ class RateLimiterMiddleware:
             if count > self.max_requests:
                 raise HTTPException(status_code=429, detail="Rate limit exceeded")
 
-            # Continue processing the request
             response = await call_next(request)
 
             return response
